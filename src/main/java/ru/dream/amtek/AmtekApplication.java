134c400613e5df24e2be5dcdd9e071d2647a605f
NEDRA\Dmitry.Gruzdev <dmitry.gruzdev@nedra.digital>
2023-02-08 12:56:40 +0300

    first commit

---
+++ src/main/java/ru/dream/amtek/AmtekApplication.java
@@ -0,0 +1,13 @@
+package ru.dream.amtek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmtekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmtekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dream/amtek/AmtekApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dream.amtek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmtekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
